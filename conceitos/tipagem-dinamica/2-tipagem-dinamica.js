c9f4d5f4eaf59f6c8e645655d9ddbc94dc5e0c69
Alex Wong de Freitas <otaru1921@gmail.com>
2021-01-09 22:00:32 -0300

    Aula 1 - Metade

---
+++ conceitos/tipagem-dinamica/2-tipagem-dinamica.js
@@ -0,0 +1,4 @@
+var meuVar = 0; // Tipo Number
+meuVar = "Texto"; // Tipo String
+
+console.log(meuVar);
---
+++ conceitos/tipagem-dinamica/2-tipagem-estatica.java
@@ -0,0 +1,14 @@
+// Código comentado porque o editor não suporta os erros que ele contém.
+
+// public class TipagemEstatica {
+//     public static void main(String[] args){
+//         int meuNumero = 10;
+//         meuNumero = "texto";
+
+//         System.out.println(meuNumero);
+//     }
+// }
+
+
+// O código acima demonstra que uma variável declarada com um tipo não pode receber um valor de outro tipo.
+// O que implica na Tipagem Estática, uma vez que não muda.
